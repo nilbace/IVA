ac8dce4ac27f9f77ffa430fbf34c8969aeb9de9e
nilbace <101455214+nilbace@users.noreply.github.com>
2023-09-07 23:43:01 +0900

    baeche

---
+++ Assets/Scripts/Managers/DataManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataManager
+{
+    #region DataManage
+
+    #endregion
+
+    #region AboutStreaming
+    public int StartDate;
+    public int nowSubCount;
+    public int nowGoldAmount;
+
+    #endregion
+
+    #region LunaStats
+    public int DrawingStat;
+    public int GamimgStat;
+    public int SingingStat;
+    public int ChattingStat;
+    public int CharmingStat;
+    public int HealthyStat;
+    public int MentalStat;
+
+    #endregion
+
+}
